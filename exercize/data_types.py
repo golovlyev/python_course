8aaf6b39a3b00fa961523f46094008884cfe25c0
Igor Golovlyev <  Golovlyev.Igor@cg.ru>
2016-03-10 16:08:42 +0300

    9 tasks solution

--- exercize/data_types.py
+++ exercize/data_types.py
@@ -10,14 +10,17 @@
     Сформировать возрастающий список из четных чисел от 2 до 10 включительно.
 """
 int_list = []
-int_list2 = []
 for i, val in enumerate(range(11)):
-    if i // 2:
+    if (i % 2 == 0) & (i != 0):
         int_list.append(val)
+# test
+assert int_list == [2, 4, 6, 8, 10]
 
-int_list = [i for i in enumerate(range(11))
-            if i // 2]
-print int_list
+int_list = [i for i, val in enumerate(range(11)) if val % 2 == 0 and val != 0]
+# test
+assert int_list == [2, 4, 6, 8, 10]
+
+int_list = [i for i in range(11) if i % 2 == 0 and i != 0]
 # test
 assert int_list == [2, 4, 6, 8, 10]
 
@@ -29,8 +32,15 @@ assert int_list == [2, 4, 6, 8, 10]
 """
 
 int_list = []
+for i, val in enumerate(reversed(range(11))):
+    if val >= 3:
+        int_list.append(val)
 # test
-# assert int_list == [10, 9, 8, 7, 6, 5, 4 , 3]
+assert int_list == [10, 9, 8, 7, 6, 5, 4, 3]
+
+int_list = [i for i in reversed(range(11)) if i >= 3]
+# test
+assert int_list == [10, 9, 8, 7, 6, 5, 4, 3]
 
 #  3
 
@@ -40,9 +50,15 @@ int_list = []
 """
 
 int_list = []
+for i, val in enumerate(reversed(range(11))):
+    if val >= 2 and val % 2 == 0:
+        int_list.append(val)
 # test
-# assert int_list == [10, 8, 6, 4, 2]
+assert int_list == [10, 8, 6, 4, 2]
 
+int_list = [i for i in reversed(range(11)) if i >= 2 and i % 2 == 0]
+# test
+assert int_list == [10, 8, 6, 4, 2]
 
 #  4
 
@@ -51,6 +67,25 @@ int_list = []
     Попробовать comprehension и цикл
 """
 
+n = 9
+m = 10
+val = 0
+matrix = []
+for r in range(n):
+    matrix.append([])
+    for c in range(m):
+        matrix[r].append(val)
+
+print("====MATRIX====")
+for r in matrix:
+    print(r)
+print("====MATRIX====")
+
+matrix = [[val for y in range(m)] for r in range(n)]
+print("====MATRIX====")
+for b in matrix:
+    print(b)
+print("====MATRIX====")
 
 #  5
 
@@ -58,11 +93,26 @@ int_list = []
     Дан список, вывести максимальный элемент в списке.
 """
 
+max_el = 0
+a = [10, 100, 345, 5467, 2, 690]
+for i in a:
+    if i > max_el:
+        max_el = i
+print 'MAX number is {0}'.format(max_el)
 
 #  6
 """
     Найти сумму 2/3 + 3/4 + 4/5 +...+ 9/10
 """
+
+a = 2.0
+b = 3.0
+sum_m = 0.0
+while a < 10 and b < 11:
+    sum_m += a / b
+    a += 1
+    b += 1
+print 'SUM is {0}'.format(sum_m)
 
 #  7
 
@@ -71,6 +121,13 @@ int_list = []
     на основе исходной
 """
 
+some_str = "aaaabbbbccccddeffff"
+result_str = ""
+for i in some_str:
+    if not result_str.__contains__(i):
+        result_str += i
+print(result_str)
+
 #  8
 
 """
@@ -78,11 +135,29 @@ int_list = []
     Попробовать dict
 """
 
+some_str = "faaaxa2bbbcc2xccddefff2ffx"
+char_dict = {}
+for i in some_str:
+    if not char_dict.has_key(i):
+        cnt = 0
+        for x in some_str:
+            if i == x:
+                cnt += 1
+        char_dict.__setitem__(i, cnt)
+print(char_dict)
 
 #  9
 """
     Удалить в строке все цифры.
 """
+
+some_str = "645236hello789 237wor894032ld"
+numbers = "0123456789"
+result_str = ""
+for i in some_str:
+    if not numbers.__contains__(i):
+        result_str += i
+print(result_str)
 
 #  10
 
